9ecdcff0fb75ed19d99ddd0437cf98db9da554da
avinashnarayana <avinash.narayana@gmail.com>
2017-05-28 22:15:15 +0000

    Redid nested routing to enable the inviter to view all invitations first.. the restful standaone routes for invitations can be added after user model is set up

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,46 @@
 class InvitationsController < ApplicationController
+  before_action :set_meeting
+  before_action :set_meeting_invitation, only: [:show, :update, :destroy]
+
+  # GET /meetings/:meeting_id/invitations
+  def index
+    json_response(@meeting.invitations)
+  end
+
+  # GET /meetings/:meeting_id/invitations/:id
+  def show
+    json_response(@invitation)
+  end
+
+  # POST /meetings/:meeting_id/invitations
+  def create
+    @meeting.invitations.create!(invitation_params)
+    json_response(@meeting, :created)
+  end
+
+  # PUT /meetings/:meeting_id/invitations/:id
+  def update
+    @invitation.update(invitation_params)
+    head :no_content
+  end
+
+  # DELETE /meetings/:meeting_id/invitations/:id
+  def destroy
+    @invitation.destroy
+    head :no_content
+  end
+
+  private
+
+  def invitation_params
+    params.permit(:name, :done)
+  end
+
+  def set_meeting
+    @meeting = meeting.find(params[:meeting_id])
+  end
+
+  def set_meeting_invitation
+    @invitation = @meeting.invitations.find_by!(id: params[:id]) if @meeting
+  end
 end
