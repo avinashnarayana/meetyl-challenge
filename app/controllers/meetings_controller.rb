17807107a89236ad1eaedafe587e2dcebfdde056
avinashnarayana <avinash.narayana@gmail.com>
2017-05-28 21:58:40 +0000

    Creating a base framework to work with for meetings and creating helper modules to handle json response and exceptions

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,43 @@
 class MeetingsController < ApplicationController
+  before_action :set_meeting, only: [:show, :update, :destroy]
+
+  # GET /meetings
+  def index
+    @meetings = meeting.all
+    json_response(@meetings)
+  end
+
+  # POST /meetings
+  def create
+    @meeting = meeting.create!(meeting_params)
+    json_response(@meeting, :created)
+  end
+
+  # GET /meetings/:id
+  def show
+    json_response(@meeting)
+  end
+
+  # PUT /meetings/:id
+  def update
+    @meeting.update(meeting_params)
+    head :no_content
+  end
+
+  # DELETE /meetings/:id
+  def destroy
+    @meeting.destroy
+    head :no_content
+  end
+
+  private
+
+  def meeting_params
+    # whitelist params
+    params.permit(:title, :created_by)
+  end
+
+  def set_meeting
+    @meeting = meeting.find(params[:id])
+  end
 end
