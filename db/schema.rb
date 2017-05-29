5a69ced6b0a590a7d7e3f0740bb0195cbf7367e9
avinashnarayana <avinash.narayana@gmail.com>
2017-05-29 23:58:05 +0000

    Added user model, defined singleton token class and AuthorizeApiRequest helper class convert token into user object

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528213656) do
+ActiveRecord::Schema.define(version: 20170529234146) do
 
   create_table "invitations", force: :cascade do |t|
     t.integer  "meeting_id"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20170528213656) do
     t.integer  "max_size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
